e8f4d3288f89399bdfcd586c376443517bd5b197
JohnMV13 <thejohnmiesterV@gmail.com>
2018-07-10 16:19:57 -0500

    John added properties to rawDataObj in the article.js

--- john-taylor/scripts/article.js
+++ john-taylor/scripts/article.js
@@ -3,11 +3,18 @@
 let articles = [];
 
 // COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// PUT YOUR RESPONSE HERE
+// The next function is where all the article information is going to go. The Reason the function is capitolized is because it is a constructor Function. "This" is the object that owns the function. rawDataObj is the variable that holds all the data for the articles.
+
 
 function Article (rawDataObj) {
   // TODO: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.publishedOn = rawDataObj.punlishedOn;
+  this.body = rawDataObj.body;
 }
 
 Article.prototype.toHtml = function() {
@@ -15,7 +22,12 @@ Article.prototype.toHtml = function() {
   // PUT YOUR RESPONSE HERE
 
   let $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+  /* TODO: This cloned article still has a class of template. In our modules.css
+   stylesheet, we should give all elements with a class of template a display of 
+   none so that our template does not display in the browser. But, we also need to 
+   make sure we're not accidentally hiding our cloned article. */
+
+  
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
@@ -28,8 +40,16 @@ Article.prototype.toHtml = function() {
       4. article body, and
       5. publication date. */
 
+  $newArticle.find("h1").text(this.title);
+  $newArticle.find("h1").text(this.category);
+  $newArticle.find("h1").text(this.author);
+  $newArticle.find("h1").text(this.authorUrl);
+  $newArticle.find("h1").text(this.publishedOn);
+  $newArticle.find("h1").text(this.body);
+
   // REVIEW: Display the date as a relative number of 'days ago'
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time')
+    .html(`about ${parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000)} days ago`);
   $newArticle.append('<hr>');
   return $newArticle;
 };
