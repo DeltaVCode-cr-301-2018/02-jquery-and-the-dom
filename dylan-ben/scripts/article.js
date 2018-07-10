61d13206d9ca1dc76dfd47f8f88df2cf94794a48
Dylan Schroeder <schrodac@gmail.com>
2018-07-10 17:02:14 -0500

    use jquery traversal/setter methods to fill template

--- dylan-ben/scripts/article.js
+++ dylan-ben/scripts/article.js
@@ -13,7 +13,7 @@ function Article (rawDataObj) {
   this.author=rawDataObj.author;
   this.authorUrl=rawDataObj.authorUrl;
   this.publishedOn=rawDataObj.publishedOn;
-  this.body=rawDataObj.body;
+  this.bodyCopy=rawDataObj.body;
 }
 
 Article.prototype.toHtml = function() {
@@ -24,7 +24,7 @@ Article.prototype.toHtml = function() {
   let $newArticle = $('article.template').clone(); //since defined, do not need parens later on.
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
   //done
-  $('article.template').removeClass('template');
+  $newArticle.removeClass('template');
   console.log($newArticle.length);
 
   if (!this.publishedOn) $newArticle.addClass('draft');
@@ -34,14 +34,16 @@ Article.prototype.toHtml = function() {
   of the current template clone with values of the properties of this 
   particular Article instance.
     We need to fill in:
-      1. author name,
-      2. author url,
-      3. article title,
-      4. article body, and
-      5. publication date. */
+      1. author name, done
+      2. author url, done
+      3. article title, done
+      4. article body,  done
+      5. publication date. done */
 
   $newArticle.find('h1').text(this.title); //find the h1 in 'this' instance of article. not all on page.
-  
+  $newArticle.find('a').text(this.author).attr('href', this.authorUrl);
+  $newArticle.find('.article-body').html(this.bodyCopy);
+  $newArticle.find('div:contains("published")').append(this.publishedOn);
 
   // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
@@ -63,5 +65,4 @@ console.log(articles);
 
 for(let i = 0; i < articles.length; i++) {
   $('#articles').append(articles[i].toHtml());
-  console.log(i);
 }
