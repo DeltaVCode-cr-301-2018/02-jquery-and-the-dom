a3361b306f0f62407d3a48726c8f8583dafe9e99
Ben Van Meter <vanmeterx@gmail.com>
2018-07-10 16:11:35 -0500

    linked javascript to html, added title to the traversal TODO

--- dylan-ben/scripts/article.js
+++ dylan-ben/scripts/article.js
@@ -8,31 +8,40 @@ let articles = [];
 function Article (rawDataObj) {
   // TODO: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
-  this.title=title;
-  this.category=category;
-  this.author=author;
-  this.authorUrl=authorUrl;
-  this.publishedOn=publishedOn;
-  this.body=body;
+  this.title=rawDataObj.title;
+  this.category=rawDataObj.category;
+  this.author=rawDataObj.author;
+  this.authorUrl=rawDataObj.authorUrl;
+  this.publishedOn=rawDataObj.publishedOn;
+  this.body=rawDataObj.body;
 }
 
 Article.prototype.toHtml = function() {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
   // .clone() makes a copy of not only the matched elements but also all the descendant elements. It is an easy way to duplicate the article elements on the page. 
 
-  let $newArticle = $('article.template').clone();
+
+  let $newArticle = $('article.template').clone(); //since defined, do not need parens later on.
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
   //done
+  $('article.template').removeClass('template');
+  console.log($newArticle.length);
+
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+  /* TODO: Now use jQuery traversal and setter methods to fill in the rest 
+  of the current template clone with values of the properties of this 
+  particular Article instance.
     We need to fill in:
       1. author name,
       2. author url,
       3. article title,
       4. article body, and
       5. publication date. */
+
+  $newArticle.find('h1').text(this.title); //find the h1 in 'this' instance of article. not all on page.
+  
 
   // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
@@ -50,7 +59,9 @@ rawData.sort(function(a,b) {
 for(let i = 0; i < rawData.length; i++) {
   articles.push(new Article(rawData[i]));
 }
+console.log(articles);
 
 for(let i = 0; i < articles.length; i++) {
   $('#articles').append(articles[i].toHtml());
+  console.log(i);
 }
